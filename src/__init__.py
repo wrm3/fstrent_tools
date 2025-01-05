9dfe6081da2c9195e1b213469013f621c9a2bc43
Warren R Martel III <wrmartel3@gmail.com>
2025-01-05 11:46:07 -0500

    Bump version: 0.6.2 → 0.6.3

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 fstrent_tools - A comprehensive collection of Python utility functions.
 """
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 # Import all functions from each module
 from .tools_convert import *
