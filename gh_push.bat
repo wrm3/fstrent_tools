84fb3ff8d8dd4f8fcd4222e8c44592917c4b8ef8
Warren R Martel III <wrmartel3@gmail.com>
2025-01-05 11:16:14 -0500

    ugh

--- gh_push.bat
+++ gh_push.bat
@@ -5,14 +5,19 @@ REM Get the bump type and commit message
 set /p BUMP_TYPE="Enter bump type (major/minor/patch) or none: "
 set /p MESSAGE="Enter commit message: "
 
-REM Add all changes and commit first
+REM Stage all changes
 git add .
-git commit -m "%MESSAGE%"
 
-REM If bump type is specified, do version bump
-if not "%BUMP_TYPE%"=="none" (
+REM If we're not bumping version, just commit and push
+if "%BUMP_TYPE%"=="none" (
+    git commit -m "%MESSAGE%"
+) else (
+    REM Commit current changes first
+    git commit -m "%MESSAGE%"
+    
+    REM Now do the version bump (working directory will be clean)
     echo Bumping %BUMP_TYPE% version...
-    python -m bumpversion --allow-dirty %BUMP_TYPE%
+    python -m bumpversion %BUMP_TYPE%
 )
 
 REM Push to main branch with tags
