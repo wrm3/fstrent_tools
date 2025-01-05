d5ca6d84e0f158c4f625b190781c186bc5656da8
Warren R Martel III <wrmartel3@gmail.com>
2025-01-05 11:28:23 -0500

    Bump version: 0.5.21 → 0.5.22

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fstrent_tools",
-    version="0.5.21",
+    version="0.5.22",
     packages=['fstrent_tools'],
     package_dir={'fstrent_tools': 'src'},
     include_package_data=True,
