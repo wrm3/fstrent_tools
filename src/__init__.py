4b02cb028ee50759f21304d7cc98acf9c84a34b2
Warren R Martel III <wrmartel3@gmail.com>
2025-01-05 09:56:48 -0500

    Bump version: 0.5.1 → 0.5.2

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@
 fstrent_tools - A comprehensive collection of Python utility functions.
 """
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 # Import all functions from each module
 from .tools_convert import *
