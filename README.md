2fcf449132628027cdbf485e8c1951d88c9869b6
Warren R Martel III <wrmartel3@gmail.com>
2025-01-05 10:32:32 -0500

    Bump version: 0.5.5 → 0.5.6

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # fstrent_tools
 
 A comprehensive collection of Python utility functions.
-version: "0.5.5"
+version: "0.5.6"
 
 ## Modules
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fstrent_tools",
-    version="0.5.5",
+    version="0.5.6",
     packages=['fstrent_tools'],
     package_dir={'fstrent_tools': 'src'},
     include_package_data=True,
